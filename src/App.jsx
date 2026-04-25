04a84aaa474a6aaab8bb00513120462da472d2e3
care-hc <care@resilient-hc.com>
2026-04-24 23:52:41 -0700

    commit

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Resilient Health Care Services</h1>
-      <h2>Wellness App</h2>
-      <p>Your app is working.</p>
-    </div>
-  );
-}
