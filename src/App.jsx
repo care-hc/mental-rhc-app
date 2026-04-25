f441ccbddff12f9426fc29096f1ccf1ecec23ca6
care-hc <care@resilient-hc.com>
2026-04-24 23:12:50 -0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Resilient Health Care Services</h1>
+      <h2>Wellness App</h2>
+      <p>Your app is working.</p>
+    </div>
+  );
+}
